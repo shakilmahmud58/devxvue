a265fbe2266a8a814abefdc41f16e60ce64a325a
Shakil Mahmud <almahmudruet17@gmail.com>
2022-09-27 07:08:55 +0600

    commited to submit

---
+++ src/components/Mycomponent.vue
@@ -0,0 +1,6 @@
+<template>
+<div class="text-center mt-5">Welcome to the home page.  <span id="msg" class="font-bold">The Nav menu will appear on the small screen.</span></div>
+</template>
+
+<style scoped>
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import "bootstrap/dist/css/bootstrap.css"
 import App from './App.vue'
 import router from './router'
 
@@ -12,3 +13,5 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+import "bootstrap/dist/js/bootstrap.js"
